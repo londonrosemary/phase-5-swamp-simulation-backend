1fe3788cb889eeb80d9f368863d146357582446f
London <london.rosemary@gmail.com>
2021-11-06 16:28:45 -0400

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts("Seeding Users...")
-u1 = User.create!(username: "london_rosemary", email: "london.rosemary@gmail.com", password: "squilliam123")
+u1 = User.create!(username: "london_rosemary", email: "london.rosemary@gmail.com", password: "squilliam123", image_url: "https://static.vecteezy.com/system/resources/previews/002/406/166/non_2x/cute-cartoon-alligator-for-kids-vector.jpg")
 puts("Seeding Pets...")
 p1 = Pet.create!(name:"King K. Rool", happiness: 65, health: 100, hunger: 60, thirst: 60, boredom: 40, user_id: u1.id)
 puts("Seeding complete!")
