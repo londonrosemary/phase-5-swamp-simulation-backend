c0698a097fc2f0272391a6fd9dfddca0e9737e5c
London <london.rosemary@gmail.com>
2021-11-09 22:28:53 -0500

    update pet#destroy

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -17,7 +17,7 @@ class PetsController < ApplicationController
     end
 
     def destroy
-        @pet = pet.find(params[:id])
+        @pet = pet.find_by(id: params[:id])
         @pet.destroy
     end
 
